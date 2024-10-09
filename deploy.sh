89ab3621bc7965076c1cb1e9e4f935ad767c53ee
Henry Wang <theKidOfArcrania@users.noreply.github.com>
2024-10-09 02:53:24 -0700

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,8 @@ wsl/README.md
 sku/efi/README.txt
 sku/efi/EFI/Boot/bootx64.efi
 sku/efi/EFI/Rufus/ntfs_x64.efi
-sku/install.ps1
+sku/install_user.ps1
+sku/install_specialize.ps1
 sku/install.sh
 sku/setup.ps1
 sku/unattend.xml
