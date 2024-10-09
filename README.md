42227ffe68943afb9218b0ba214646b9160afd3a
Henry Wang <theKidOfArcrania@users.noreply.github.com>
2024-10-09 11:34:34 -0700

    Make more fixes to scheduled task

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Description: Arghh dammit, so my code is insecure, but surely you can't root
 my box too right?
 
 Note from author: please be nice to the instances. There are a couple of shared
-instances used for the first part, please don't ruin it for others :) . You can
-also connect to a unique instance if you give the first part flag, but instances
-will be severely limited.
+instances used for part I, please don't ruin it for others :) . You can also
+connect to a unique instance if you give the first part flag, but instances will
+be severely limited. The unique instances will take a while to boot (on the
+order of a few minutes. Don't worry, they will eventually load).
--- sku/install_user.ps1
+++ sku/install_user.ps1
@@ -5,11 +5,11 @@ debian install --root
 debian run /mnt/c/OEM/install.sh
 debian config --default-user ctf
 
-$action = New-ScheduledTaskAction -Execute (where.exe debian) -Argument "run bash -c /home/ctf/runner update"
-$principal = New-ScheduledTaskPrincipal -RunLevel Highest
+$action = New-ScheduledTaskAction -Execute (where.exe debian) -Argument "run /home/ctf/runner update"
+$principal = New-ScheduledTaskPrincipal -UserId ctf -RunLevel Highest
 $trigger = New-ScheduledTaskTrigger `
   -Once `
   -At (Get-Date) `
   -RepetitionInterval (New-TimeSpan -Minutes 1) `
-  -RepetitionDuration ([System.TimeSpan]::MaxValue)
+  -RepetitionDuration (New-TImeSpan -Days 50)
 Register-ScheduledTask -Trigger $trigger -Action $action -Principal $principal -TaskName "update_wsl"
